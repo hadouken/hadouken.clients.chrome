b903586b24290b98bd86954e9ff9323c4e3ce467
Viktor Elofsson <viktor@viktorelofsson.se>
2014-03-12 19:05:16 +0100

    Grunt now builds a sweet zip-file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,21 @@
 module.exports = function(grunt) {
-    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        compress: {
+            main: {
+                options: {
+                    archive: 'build/hadouken.extensions.chrome.zip'
+                },
+                files: [
+                    { src: ['**/*.*'], dest: '/', cwd: 'src/', expand: true }
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-compress');
+
+    // Default task should be minify, compress
+    grunt.registerTask('default', ['compress']);
 };
