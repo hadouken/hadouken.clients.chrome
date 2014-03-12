c337df8f86452173e7544f9b2c7890bdd23278e8
Viktor Elofsson <viktor@viktorelofsson.se>
2014-03-12 19:34:43 +0100

    Gruntfile now builds everything correctly

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,66 @@
 module.exports = function(grunt) {
+    var version = '0.0.0';
+
+    if(typeof process.env.VERSION !== 'undefined') {
+        version = process.env.VERSION;
+    }
+
     grunt.initConfig({
+        version: version,
+
         pkg: grunt.file.readJSON('package.json'),
+
+        clean: [
+            'build/'
+        ],
+
+        copy: {
+            main: {
+                files: [
+                    { src: ['**/*.*'], dest: 'build/hdkn.chrome-<%= version %>/', cwd: 'src/', expand: true }
+                ]
+            }
+        },
+
+        setversion: {
+            main: {
+                options: {
+                    manifest_file: 'build/hdkn.chrome-<%= version %>/manifest.json',
+                    version: '<%= version %>'
+                }
+            }
+        },
 
         compress: {
             main: {
                 options: {
-                    archive: 'build/hadouken.extensions.chrome.zip'
+                    archive: 'build/hdkn.chrome-<%= version %>.zip'
                 },
                 files: [
-                    { src: ['**/*.*'], dest: '/', cwd: 'src/', expand: true }
+                    { src: ['**/*.*'], dest: '/', cwd: 'build/hdkn.chrome-<%= version %>/', expand: true }
                 ]
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-compress');
 
-    // Default task should be minify, compress
-    grunt.registerTask('default', ['compress']);
+    // Default task should be clean, copy, setversion, compress
+    grunt.registerTask('default', ['clean', 'copy', 'setversion', 'compress']);
+
+    // setversion task
+    grunt.registerMultiTask('setversion', 'Sets the version in the manifest.json file', function() {
+        var options = this.options({
+            manifest_file: 'manifest.json',
+            version: '0.0.0'
+        });
+
+        var manifest = grunt.file.readJSON(options.manifest_file);
+        manifest.version = options.version;
+
+        // Write new manifest file
+        grunt.file.write(options.manifest_file, JSON.stringify(manifest, undefined, 2));
+    });
 };
